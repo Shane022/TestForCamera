db7acae7638112e171e6fce178d268152b0d6ec2
shangzhixin <shangzhixin@sumaplayer.com>
2016-08-12 16:02:25 +0800

    synx project

---
+++ TestForCamera/CommonDefine.h
@@ -0,0 +1,13 @@
+//
+//  CommonDefine.h
+//  TestForCamera
+//
+//  Created by dvt04 on 16/8/12.
+//  Copyright © 2016年 suma. All rights reserved.
+//
+
+#ifndef CommonDefine_h
+#define CommonDefine_h
+
+
+#endif /* CommonDefine_h */
